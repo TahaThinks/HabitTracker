4a51c9091a8272ea97041fe006e67530579b1a40
TahaThinks <tahaa4587@gmail.com>
2024-05-13 10:58:39 +0400

    pixela parameters

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import requests
 
 pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    'token': "tahalearns",
+    'username': "Taha",
+    'agreeTermsOfService': "yes",
+    'notMinor': 'yes'
+}
