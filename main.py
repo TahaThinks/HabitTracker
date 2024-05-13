9cbe771902f11e3673d1ddd64f85f7c6151567df
TahaThinks <tahaa4587@gmail.com>
2024-05-13 11:08:35 +0400

    Code Refactor

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import requests
 
+USERNAME = "tahaleatnspixela"
+TOKEN = "tahalearnspixela17041996"
+
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    'token': "tahalearnspixela17041996",
-    'username': "tahalearnspixela",
+    'token': TOKEN,
+    'username': USERNAME,
     'agreeTermsOfService': "yes",
     'notMinor': 'yes'
 }
 
-r = requests.post(url=pixela_endpoint, json=user_params)
-r.raise_for_status()
-print(r.text)
+# r = requests.post(url=pixela_endpoint, json=user_params)
+# r.raise_for_status()
+# print(r.text)
+
