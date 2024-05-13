9d7be6306a957edd37f51af508fe55f3f4ba8408
TahaThinks <tahaa4587@gmail.com>
2024-05-13 11:06:31 +0400

    pixela response

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import requests
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    'token': "tahalearns",
-    'username': "Taha",
+    'token': "tahalearnspixela17041996",
+    'username': "tahalearnspixela",
     'agreeTermsOfService': "yes",
     'notMinor': 'yes'
 }
+
+r = requests.post(url=pixela_endpoint, json=user_params)
+r.raise_for_status()
+print(r.text)
